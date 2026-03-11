e5a052e894fc638d255191c9a6bac290dfe9e69b
ASC <apenasmast@gmail.com>
2026-03-11 15:59:48 -0300

    Delete veritabani.sql
    Signed-off-by: ASC <apenasmast@gmail.com>

--- veritabani.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS makaleler (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    baslik TEXT NOT NULL,
-    slug TEXT NOT NULL,
-    ozet TEXT,
-    icerik TEXT NOT NULL,
-    kapak_resmi TEXT,
-    tarih DATETIME DEFAULT CURRENT_TIMESTAMP
-);
